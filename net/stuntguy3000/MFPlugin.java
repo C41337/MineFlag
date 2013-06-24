7d84d366d75eda73cadec174237bbfdc79886384
keto23 <kethomassen@gmail.com>
2013-06-24 21:36:54 +1000

    Merge from BitBucket

---
+++ net/stuntguy3000/MFPlugin.java
@@ -0,0 +1,28 @@
+package net.stuntguy3000;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MFPlugin extends JavaPlugin {
+	
+	Logger log = Bukkit.getLogger();
+	
+	public void onEnable() {
+		debug("Starting config copy...");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		debug("...Complete.");
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void debug(String msg)
+	{
+		log.info("[MineFlag Debug`] " + msg);
+	}
+	
+}
