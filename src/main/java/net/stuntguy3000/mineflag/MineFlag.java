078529ccf5e7ae6db9029b238e51f7ec309237f7
keto23 <kethomassen@gmail.com>
2013-06-24 21:50:59 +1000

    Maven

---
+++ src/main/java/net/stuntguy3000/mineflag/MineFlag.java
@@ -0,0 +1,16 @@
+package net.stuntguy3000.mineflag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineFlag extends JavaPlugin {
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
+
