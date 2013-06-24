ac81be8649559ab3121f3359d55140438ac97a83
keto23 <kethomassen@gmail.com>
2013-06-24 22:43:35 +1000

    Revert Back to Maven

---
+++ src/main/java/net/stuntguy3000/mineflag/MineFlag.java
@@ -0,0 +1,15 @@
+package net.stuntguy3000.mineflag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineFlag extends JavaPlugin {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+
+    }
+
+}
+
